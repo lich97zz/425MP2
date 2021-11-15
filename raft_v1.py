ed2ce11b9ec3d4adb52c33f9afaa81c6a8c0dd2c
lich97zz <136956344@qq.com>
2021-11-15 11:32:37 -0600

    1

--- raft_v1.py
+++ raft_v1.py
@@ -241,10 +241,11 @@ class Raft:
                             while len(self.log) > ind-1:
                                 self.log = self.log[:-1]
                                 self.logcontent = self.logcontent[:-1]
-                            
+                            content[i] = content[i][1:-1] #remove the leading and tail '
                             print("****info, logpushing:",entry[i],' ',content[i],' at ',self.pid)
                             self.log.append(entry[i])
                             self.logcontent.append(content[i])
+                            
                             print('STATE log['+str(len(self.log))+']=['+str(self.term)+',"'+content[i]+'"]' )
                     matchId = ind
 
