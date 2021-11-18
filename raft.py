37996b768ea0fdfd862ac9f66e3217f0247cbdf2
lich97zz <136956344@qq.com>
2021-11-17 21:42:56 -0600

    1

--- raft.py
+++ raft.py
@@ -22,7 +22,6 @@ l=threading.Lock()
 
 class Raft:
     def __init__(self,n,pid):
-        print("entering Raft init")
         self.state='"FOLLOWER"'
         print(f'STATE',f'state={self.state}')
 
@@ -31,39 +30,32 @@ class Raft:
         self.leader=None
 
         self.log=[]#modify2
+        self.logcontent=[]
         self.commitId = 0
         self.matchId=dict()
         self.nextId=dict()
-##        self.rpcDue=dict()
-##        self.hbDue=dict()
         for i in range(n):
             self.matchId[i]=0
             self.nextId[i]=1
-##            self.rpcDue[i]=0
-##            self.hbDue[i]=0
         #end of modify2
-        
+            
         self.votedFor=pid
         self.term=0
         print(f'STATE',f'term={self.term}')
 
         self.voteGranted=[False]*n
         self.ELECTION_TIMEOUT=1
-
         self.timer=None
         self.resetTimer()
-
         self.heartbeat=None
-
+        
     def logTerm(self, log, ind):
         if ind<1 or ind>len(log):
             return 0
-        return log[ind-1].term
-    
+        return log[ind-1]
+
     def resetTimer(self):
-        
         #fix concurrency issue
-        
         self.timer=threading.Timer(self.ELECTION_TIMEOUT*(1+random()),self.timeoutHandlerThread)
         self.timer.start()
 
@@ -71,32 +63,19 @@ class Raft:
     def stepdown(self,term):
         self.term=term
         print(f'STATE',f'term={self.term}')
+
+        self.state='"FOLLOWER"'
+        print(f'STATE',f'state={self.state}')
         
         self.votedFor=None
         self.voteGranted=[False]*self.n
-        self.state='"FOLLOWER"'
-        print(f'STATE',f'state={self.state}')
         #todo: check this in heartbeat thread
         #  if self.heartbeat:
         #     self.heartbeat.cancel()
         
-
     def send(self,destpid,*args):
-##modify3
-##self.send(i,'RequestVotes',self.term,lastLogTerm,LastLogId)
-##self.send(i,'AppendEntries',self.term, prevId, prevTerm, entry, self.commitId)
-##        print('SEND',destpid,*args,flush=True)
-        if len(args) < 2:
-            return
-        msgtype = args[1]
-        if msgtype == 'RequestVotes':
-            #todo, don't need so many parameters
-            print('SEND',destpid,args,flush=True)
-        elif msgtype == 'AppendEntries':
-            #todo
-            print('SEND',destpid,args,flush=True)
-            
-        
+        print('SEND',destpid,*args,flush=True)
+    
     def becomeLeader(self):
         self.state='"LEADER"'
         print(f'STATE',f'state={self.state}')
@@ -104,48 +83,73 @@ class Raft:
         self.leader=self.pid
         print(f'STATE',f'leader={self.leader}')
 
-        #modify3
-        for i in range(n):
-            if i == self.pid:
-                continue
-            self.nextId[i] = len(self.log)+1
-            
         self.heartbeat=threading.Thread(target=self.heartbeatThread,args=(self.term,))
         self.heartbeat.start()
 
+    def printinfo(self):
+        infodict = dict()
+        infodict['n']=self.n
+        infodict['pid']=self.pid
+        infodict['leader'] = self.leader
+        infodict['log'] = self.log
+        infodict['logcontent'] = self.logcontent
+        infodict['commitId'] = self.commitId
+        infodict['matchId'] = self.matchId
+        infodict['nextId'] = self.nextId
+        print("info:",infodict)
+        
+    def updateCommit(self):
+        matchIdTmp = list(self.matchId.values())
+        matchIdTmp[self.pid] = len(self.log)
+        matchIdTmp.sort()
+        ind = matchIdTmp[int(self.n/2)]
+        if self.state=='"LEADER"' and self.logTerm(self.log,ind)==self.term:
+            oldCommitId = self.commitId
+            self.commitId = max(self.commitId, ind)
+            if self.commitId > oldCommitId:
+                print('STATE commitIndex='+str(self.commitId))
+                for i in range(oldCommitId+1, self.commitId+1):
+                    if i > len(self.log):
+                        break
+                    print('COMMITTED '+str(self.logcontent[i-1])+' '+str(i))
+        
     def processmsg(self,msg):
         msg=msg.split()
-                
-        #modify2, LOG msg
+        
         if msg[0]=='LOG':
             content=msg[1]
-            self.log.append(content)
+##            print("************content,",content)
+##            print("************content,",msg)
+            self.logcontent.append(content)
+            self.log.append(self.term)
             print('STATE log['+str(len(self.log))+']=['+str(self.term)+',"'+content+'"]' )
+##            if(len(self.log)>=3):
+##                print("************content,",self.log)
+##                print("************content,",self.logcontent)
             return
-
-        #modify
-        if len(msg) < 4:
-            return
-        
+    
         srcpid=int(msg[1])
         msgtype=msg[2]
-        term=int(msg[3])
         
+        term=int(msg[3])
+
         if self.term<term:
             self.stepdown(term)
 
         if msgtype=='RequestVotes':
-            print("entering requestVotes handler")
+            
+##            print("entering requestVotes handler")
 ##self.send(i,'RequestVotes',self.term,lastLogTerm,LastLogId)
             #modify
-            lastLogTerm = msg[4]
-            lastLogId = msg[5]
+##            lastLogTerm = int(msg[4])
+##            lastLogId = int(msg[5])
             
             agree=False
             
             if self.term==term and (self.votedFor in {None,srcpid}):
-                #modify
-##                cond1 = (lastLogTerm>self.logTerm(self.log,len(self.log)))
+              #modify4  
+##                print("****info:",lastLogTerm,self.logTerm(self.log,len(self.log)))
+##                cond1 = (lastLogTerm > self.logTerm(self.log,len(self.log)))
 ##                cond2 = (lastLogTerm==self.logTerm(self.log,len(self.log))) and (lastLogId>len(self.log))
 ##                if cond1 or cond2:
 ##                
@@ -169,64 +173,81 @@ class Raft:
                 self.voteGranted[srcpid]=agree
                 if self.voteGranted.count(True) > self.n//2:
                     self.becomeLeader()
-
+                    
         if msgtype=='AppendEntries':
-##            
-##      self.send(i,'AppendEntries',self.term, prevId, prevTerm, entry, commitId)
-            #modify
-            prevId = msg[4]
-            prevTerm = msg[5]
-            entry = msg[6]
-            commitId = msg[7]
+            
+            prevId = int(msg[4])
+            prevTerm = int(msg[5])
+            
+            tmp = msg[6][1:-1].split(',')
+            entry = []
+            for elm in tmp:
+                if elm == '':
+                    continue
+                entry.append(int(elm))
 
+            tmp = msg[7][1:-1].split(',')
+            content = []
+            for elm in tmp:
+                if elm == '':
+                    continue
+                content.append(elm)
+                
+            commitId = int(msg[8])
+            
             success=False
             matchId = 0
 
             if self.term==term:
-                self.resetTimer()
                 self.state='"FOLLOWER"'
                 print(f'STATE',f'state={self.state}')
-                
+                self.resetTimer()
                 if self.leader!=srcpid:
                     self.leader=srcpid
                     print(f'STATE',f'leader={self.leader}')
-                
-            #modify3
+
                 cond1 = (prevId==0)
                 cond2 = (prevId<=len(self.log) and (self.logTerm(self.log, prevId)==prevTerm))
                 if cond1 or cond2:
                     success=True
                     ind = prevId
+                    
                     for i in range(len(entry)):
                         ind+=1
-                        if self.logTerm(self.log, ind) != entry[i].term:
-                            while len(self.log) >= ind:
+                        if self.logTerm(self.log, ind) != entry[i]:
+                            while len(self.log) > ind-1:
                                 self.log = self.log[:-1]
-                            self.log.push(entry[i])
+                                self.logcontent = self.logcontent[:-1]
+                            content[i] = content[i][1:-1] #remove the leading and tail '
+                            self.log.append(entry[i])
+                            self.logcontent.append(content[i])
+                            print('STATE log['+str(len(self.log))+']=['+str(self.term)+',"'+content[i]+'"]' )
+
                     matchId = ind
+                    oldCommitId = self.commitId
                     self.commitId = max(self.commitId, commitId)
-                
-            
+                    if self.commitId > oldCommitId:
+                        print('STATE commitIndex='+str(self.commitId))
+                        for i in range(oldCommitId+1, self.commitId+1):
+                            if i > len(self.log):
+                                break
+                            print('COMMITTED '+str(self.logcontent[i-1])+' '+str(i))
+
             self.send(srcpid,'AppendEntriesResponse',self.term,success, matchId)
 
         if msgtype=='AppendEntriesResponse':
-##  self.send(srcpid,'AppendEntriesResponse',self.term,success, matchId)
             if self.state!='"LEADER"':
                 return
             if self.term != term:
                 return
             agree = (msg[4]=='True')
-            matchId = msg[5]
+            matchId = int(msg[5])
             if agree:
                 self.matchId[srcpid] = max(self.matchId[srcpid], matchId)
                 self.nextId[srcpid] = matchId+1
+                self.updateCommit()
             else:
                 self.nextId[srcpid] = max(1, self.nextId[srcpid]-1)
-                
-   
-    
-    
-
 
     def msgHandler(self):
         while msg:=sys.stdin.readline():
@@ -234,34 +255,35 @@ class Raft:
             self.processmsg(msg)
             l.release()
 
-
-
     def heartbeatThread(self,term):
         l.acquire()
         #modify3, heartbeat time reset
-        while self.term==term:
+        while self.term==term and self.state=='"LEADER"':
             for i in range(self.n):
                 if i!=self.pid:
                     #todo
-##                    if self.nextId[i] > len(self.log):
-##                        continue
+                    #if HeartBeat timeout, or self.nextId[i] <= len(self.log), do the following...
+
                     prevId = self.nextId[i] - 1
-                    lastId = len(self.log)
-                    if self.matchId[i] <= self.nextId[i]:
+                    lastId = min(prevId+1, len(self.log))
+                    if self.matchId[i] < self.nextId[i]-1:
                         lastId = prevId
                     prevTerm = self.logTerm(self.log, prevId)
                     entry = self.log[prevId:lastId]
+                    content = self.logcontent[prevId:lastId]
                     commitId = min(self.commitId, lastId)
-                    self.send(i,'AppendEntries',self.term, prevId, prevTerm, entry, commitId)
+                    self.send(i,'AppendEntries',self.term, prevId, prevTerm, entry, content, commitId)
             l.release()
             time.sleep(self.ELECTION_TIMEOUT/4)
             l.acquire()
         l.release()
 
     def timeoutHandlerThread(self):
-        print("in timeout handler thread")
+        if self.state == '"LEADER"':
+            return
         l.acquire()
         if self.timer==threading.current_thread():
+##            print("***********timeout")
             self.resetTimer()
             self.term+=1
             print(f'STATE',f'term={self.term}')
@@ -275,18 +297,18 @@ class Raft:
             for i in range(self.n):
                 if i!=self.pid:
                     #modify3
-                    lastLogTerm = self.logTerm(self.log, len(self.log))
-                    lastLogId = len(self.log)
-                    print("sending request Votes,",self.term,lastLogTerm,lastLogId)
-                    self.send(i,'RequestVotes',self.term,lastLogTerm,lastLogId)
-##                    self.send(i,'RequestVotes',self.term)
-
+##                    lastLogTerm = self.logTerm(self.log, len(self.log))
+##                    lastLogId = len(self.log)
+##                    print("***info1",lastLogTerm)
+##                    self.send(i,'RequestVotes',self.term,lastLogTerm,lastLogId)
+                    self.send(i,'RequestVotes',self.term)
         l.release()
 
 
-print("Init start")
+
 r=Raft(n,pid)
-print("Init finished")
+
+
 r.msgHandler()
 
 
